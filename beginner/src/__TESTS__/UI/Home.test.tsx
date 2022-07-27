88e75245c9bb24ab5d9d36d96cf45ab2c4bf01e4
emarson.halcyondigital <emarson.halcyondigital@gmail.com>
2022-07-27 11:28:51 +0800

    chore: beginner re run

--- beginner/src/__TESTS__/UI/Home.test.tsx
+++ beginner/src/__TESTS__/UI/Home.test.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Home from '../../Home';
 
-describe('render a component with child props', () => {
-  test('home to render with a prop', () => {
-    render(<Home name='jonel' />);
-    const elementWithProp = screen.getByText(/hello my name is bulagtok/i);
+describe('component', () => {
+  it('render with props', () => {
+    render(<Home name='prop' />);
+    const elementWithProp = screen.getByText(/hello my name is propw/);
     expect(elementWithProp).toBeInTheDocument();
   });
 });
-// As expected this fails in the runtime since the prop passed is not the same
